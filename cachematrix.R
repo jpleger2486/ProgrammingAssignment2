7c96cd15927bb57a11ea619c72fbe97a7e038474
jpleger2486 <jpleger@JPs-MacBook-Pro.local>
2015-04-24 15:41:50 -0400

    Updated for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## This is a pair of functions used to invert matrices with the 
+## extra feature of cachig previously inverted matrices to improve performance.
+## This is straight rewite of makeVector and cachemean provided in github
+## https://github.com/rdpeng/ProgrammingAssignment2
+## by Prof. Peng of Johns Hopkins University
+## 
+## Usage:
+## Load the two functions.
+## Create an invertable matrix and cache it with makeCacheMatrix
+## this essentially turs the matrix into an object with getters and setters
+## that can be later reused.
+## Call cacheSolve to invert the matrix.
+## The first time the matrix will be inverted, the second and subsequent times
+## The cached inverted matrix wll be used
+##
+## Example
+## > c <- matrix(1:4, 2, 2)
+## > c
+## [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
+## > z <- makeCacheMatrix(c)
+## > cacheSolve(z)
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheSolve(z)
+## getting cached inverse
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a list of functions used to persist an inverted matrix
+## It is esentially making an object of the inverted matrix with getter and setters
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+            x <<- y
+            m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is passed a matrix to invert 
+## It invertets the matrix and uses makeCacheMatrix methods to persist 
+## the inverted matrix.  If the passed in matrix has already been persisted,
+## the persisted copy is returned.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+            message("getting cached inverted matrix")
+            return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
